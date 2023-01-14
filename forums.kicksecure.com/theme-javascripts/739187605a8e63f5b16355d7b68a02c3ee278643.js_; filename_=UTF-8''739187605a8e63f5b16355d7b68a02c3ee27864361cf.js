131f84242761901038215954a0103b588acf4af7
root <root@kicksecure.com>
2023-01-14 02:10:15 +0000

    2023 01 14 | Sat 14 Jan 2023 02:10:15 AM UTC | automatic backup

--- forums.kicksecure.com/theme-javascripts/739187605a8e63f5b16355d7b68a02c3ee278643.js_; filename_=UTF-8''739187605a8e63f5b16355d7b68a02c3ee27864361cf.js
+++ forums.kicksecure.com/theme-javascripts/739187605a8e63f5b16355d7b68a02c3ee278643.js_; filename_=UTF-8''739187605a8e63f5b16355d7b68a02c3ee27864361cf.js
@@ -1,73 +1,97 @@
 
-    (function() {
-  
-      let contentForSlides = `
-        <h1>We want to <b>grow and improve</b> - please help Kicksecure!</h1>
-        
-        <div data-crypto-addresses='{
-          "payBitcoin": "3DaJWfHyLv4RVnvMD7K2Mz2AX2r3fwiQwV",
-          "payMonero": "84ozcSohQfoV6nRgGfaQ8uBvWphXAH8zDTTuotVnJWF1JMNQfvgNFdbEo4ZnJ9hxPMeYfJuUoWGH3MRaXCfbYk8sFFgm4XL",
-          "payEthereum": "0xf27EAe399f186600Dc6e5A418793C4A3D58a74e7"
-        }'>
-        </div>
-        
-        <div>
-          <img src="/w/images/3/30/End-of-year-green.jpg" />
-          Kicksecure is an Open Source security power house and the basis for <a target="_blank" href="https://www.whonix.org">Whonix</a> - <b>providing great value to thousands of users</b>! 
-          But only those who move forward continue to thrive. Would you support our next big leap? 
-        </div>
-        
-        <div>
-          <img src="/w/images/2/2d/End-of-year-blue.jpg" />
-          For 2023 we're <b>improving the usability and enhancing the user experience</b>. We're also hardening the kernel against attacks, improving access control, adding a sandbox and many more features.
-        </div>
-        
-        <div>
-          <img src="/w/images/e/ef/End-of-year-yellow.jpg" />
-          We aim to create a high impact outreach with articles and videos to attract more users, get more feedback and in turn improve security for YOU!
-          <b>Would you consider a donation to keep Kicksecure independent</b> and help reach these new goals? Thank you.
-        </div>
-      `;
-          
-      let initBanner = () => {
-        
-        // Will not be executed after 2022-31-12 23:59
-        if( Date.now() > 1672527599000 ) {
-          return; 
-        }
-      
-        // If the user has the dismissed less than a week ago, it will not be executed
-        if( localStorage.getItem('dismiss-end-of-year-sitenotice') > Date.now() ) {
-            $('#siteNotice').addClass('hide-notice');
-            return;
-        }
-        // Else remove item (because it's not valid) if it exists
-        localStorage.removeItem('dismiss-end-of-year-sitenotice');
-    
-        $('#main-container').append( '<div id="end-of-year-banner-wrapper"></div>');
-        
-        let url = ( window.location.href.match(/\.onion/) ? 'http://w5j6stm77zs6652pgsij4awcjeel3eco7kvipheu6mtr623eyyehj4yd.onion' : 'https://www.kicksecure.com' );
-        
-    
-        initEndOfYearSitenotice( $('#end-of-year-banner-wrapper'), url, contentForSlides );
-    
-        let eoy = $('#donate-end-of-year');
-        eoy.find('.btc .cbutton').attr('src','/w/images/thumb/2/29/BC_Logo_.png/40px-BC_Logo_.png')
-        eoy.find('.xmr .cbutton').attr('src','/w/images/thumb/0/05/Monero-symbol-1280.png/40px-Monero-symbol-1280.png')
-        eoy.find('.eth .cbutton').attr('src','/w/images/thumb/2/2c/Ethereumlogo.png/40px-Ethereumlogo.png')
-    
-        $('#end-of-year-banner-modal').animate( { opacity: 1 }, 1000 );
-        
-        eoy.find('.dismiss').on('click', function() {
-			$('#end-of-year-banner-wrapper').animate({ height: 0, opacity: 0 }, 1000, function() {
-				$(this).attr('style','display:none !important;');
-			});
-		});
-    
-      };
-      
-      $(document).ready( initBanner );
-      
-    })();
 
+<div class="page-not-found">
+  <h1 class="title">Oops! That page doesn’t exist or is private.</h1>
+
+    <a href="/login" class='btn btn-primary'><svg class="fa d-icon svg-icon svg-node" aria-hidden="true"><svg id="user" viewBox="0 0 448 512">
+    <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/>
+  </svg></svg>
+Log In</a>
+
+</div>
+
+
+
+<div class="row page-not-found-topics">
+    <div class="popular-topics">
+      <h2 class="popular-topics-title">Popular</h2>
+        <div class='not-found-topic'>
+          <a href="/t/using-tirdad-with-fsf-libre-kernel/188">Using Tirdad with FSF libre kernel</a><a class='badge-wrapper  bullet' href='/c/qubes/8'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >Qubes</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/no-valid-pgp-data-found/186">No valid PGP data found</a><a class='badge-wrapper  bullet' href='/c/website/2'><span class='badge-category-bg' style='background-color: #808281;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='Issues and improvements with the Kicksecure website.'>Website</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/kicksecure-16-0-9-0-kvm-point-release/191">Kicksecure 16.0.9.0 KVM - Point Release!</a><a class='badge-wrapper  bullet' href='/c/news/5'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >News</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/domain-clientdeleteprohibited-status/179">Domain clientDeleteProhibited status</a><a class='badge-wrapper  bullet' href='/c/website/2'><span class='badge-category-bg' style='background-color: #808281;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='Issues and improvements with the Kicksecure website.'>Website</span></a>
+        </div>
+      <a href="/top" class="btn btn-default">More&hellip;</a>
+    </div>
+    <div class="recent-topics">
+      <h2 class="recent-topics-title">Recent</h2>
+        <div class='not-found-topic'>
+          <a href="/t/mullvadvpn-and-opensnitch-on-kicksecure/195">MullvadVPN and OpenSnitch on Kicksecure</a><a class='badge-wrapper  bullet' href='/c/support/6'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='User support for the Kicksecure operating system.'>Support</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/kicksecure-16-0-9-0-kvm-point-release/191">Kicksecure 16.0.9.0 KVM - Point Release!</a><a class='badge-wrapper  bullet' href='/c/news/5'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >News</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/using-tirdad-with-fsf-libre-kernel/188">Using Tirdad with FSF libre kernel</a><a class='badge-wrapper  bullet' href='/c/qubes/8'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >Qubes</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/no-valid-pgp-data-found/186">No valid PGP data found</a><a class='badge-wrapper  bullet' href='/c/website/2'><span class='badge-category-bg' style='background-color: #808281;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='Issues and improvements with the Kicksecure website.'>Website</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/domain-clientdeleteprohibited-status/179">Domain clientDeleteProhibited status</a><a class='badge-wrapper  bullet' href='/c/website/2'><span class='badge-category-bg' style='background-color: #808281;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='Issues and improvements with the Kicksecure website.'>Website</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/trouble-installing-kicksecure-qubes-gui-in-qubes/170">Trouble installing kicksecure-qubes-gui in qubes</a><a class='badge-wrapper  bullet' href='/c/support/6'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='User support for the Kicksecure operating system.'>Support</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/cannot-use-split-u2f-yubikey-website-authentication/165">Cannot use split-u2f (yubikey website authentication)</a><a class='badge-wrapper  bullet' href='/c/qubes/8'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >Qubes</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/kicksecure-16-0-9-0-for-virtualbox-point-release/160">Kicksecure 16.0.9.0 - for VirtualBox - Point Release!</a><a class='badge-wrapper  bullet' href='/c/news/5'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >News</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/degree-of-anonymity-kicksecure-vs-whonix-on-qubes-os/159">Degree of anonymity Kicksecure vs Whonix on Qubes-OS</a><a class='badge-wrapper  bullet' href='/c/qubes/8'><span class='badge-category-bg' style='background-color: #0088CC;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 >Qubes</span></a>
+        </div>
+        <div class='not-found-topic'>
+          <a href="/t/adding-new-packages-will-be-installed-with-its-recommends/156">Adding new packages will be installed with its recommends</a><a class='badge-wrapper  bullet' href='/c/development/7'><span class='badge-category-bg' style='background-color: #25AAE2;'></span><span style='' data-drop-close='true' class='badge-category clear-badge'
+                 title='Development of Kicksecure software and source code.'>Development</span></a>
+        </div>
+      <a href="/latest" class="btn btn-default">More&hellip;</a>
+    </div>
+</div>
+
+
+  <div class="row">
+    <div class="page-not-found-search">
+      <h2>Search this site</h2>
+      <p>
+        <form action='/search' id='discourse-search'>
+          <input type="text" name="q" value="">
+          <button class="btn btn-primary">Search</button>
+        </form>
+      </p>
+    </div>
+  </div>
+
+  <link rel="preload" href="/assets/onpopstate-handler-ece5c756a2e952d484f91ce9faefcf87d7660f2102e26047221987c335ff8c11.js" as="script">
+<script defer src="/assets/onpopstate-handler-ece5c756a2e952d484f91ce9faefcf87d7660f2102e26047221987c335ff8c11.js"></script>
 
